b58922cafb93e4a149c3f2e5fb4d9f1f96ea134f
Kaan <yusufkaan142@gmail.com>
2019-08-04 09:17:11 +0000

    added total ram count remove unusef imports

--- nodeinfo.go
+++ nodeinfo.go
@@ -3,20 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"runtime"
-	"strings"
 	"syscall"
 	"time"
 )
 
 func main() {
 
-	/*t, err := getLastSystemBootTime()
-	if err != nil {
-		panic(err)
-	}*/
-	//uptime := time.Now().Unix() - t.Unix()
 	hostname, _ := os.Hostname()
 	si := &syscall.Sysinfo_t{}
 
@@ -31,6 +24,8 @@ func main() {
 	fmt.Println("Platform:  ", runtime.GOOS)
 	fmt.Println("Arch:      ", runtime.GOARCH)
 	fmt.Println("CPU count: ", runtime.NumCPU())
+	fmt.Println("Total Ram: ", ByteCountIEC(uint64(si.Totalram) * uint64(si.Unit)))
+	
 
 	days := uptime / (60 * 60 * 24)
 	hours := (uptime - (days * 60 * 60 * 24)) / (60 * 60)
@@ -38,25 +33,16 @@ func main() {
 	fmt.Printf("Uptime:     %d days, %d hours, %d minutes \n", days, hours, minutes)
 }
 
-func getLastBootTime() string {
-	out, err := exec.Command("who", "-b").Output()
-	if err != nil {
-		panic(err)
-	}
-	t := strings.TrimSpace(string(out))
-	t = strings.TrimPrefix(t, "system boot")
-	t = strings.TrimSpace(t)
-	return t
-}
-
-func getTimezone() string {
-	out, err := exec.Command("date", "+%Z").Output()
-	if err != nil {
-		panic(err)
-	}
-	return strings.TrimSpace(string(out))
-}
-
-func getLastSystemBootTime() (time.Time, error) {
-	return time.Parse(`2006-01-02 15:04MST`, getLastBootTime()+getTimezone())
+func ByteCountIEC(b uint64) string {
+    const unit = 1024
+    if b < unit {
+        return fmt.Sprintf("%d B", b)
+    }
+    div, exp := int64(unit), 0
+    for n := b / unit; n >= unit; n /= unit {
+        div *= unit
+        exp++
+    }
+    return fmt.Sprintf("%.1f %ciB",
+        float64(b)/float64(div), "KMGTPE"[exp])
 }
